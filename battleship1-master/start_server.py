5bb1492711da6ebcf56ac2b43adc7f67dbc085ad
Sergey Beglov <doktorkrab@github.com>
2016-07-13 14:23:39 +0300

    Очередь + обработка выхода

--- battleship1-master/start_server.py
+++ battleship1-master/start_server.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-import sys,vk, threading, time
+import sys,vk, threading, time, os
 from main_server import *
 from my_algo import *
 # Чтение токена
+lastgame = 0
 token = open('token_server.txt','r')
 gg = token.read()
 api = vk.API(vk.Session(access_token=gg[:-1]))
@@ -25,33 +26,40 @@ except FileNotFoundError:
     users_file.close()                 #
 except Exception as e:
     print('Необработанное исключение!\n Текст:\n',e) # Защита от вылетов
-while True:
+try:
     while True:
-        try:
-            messages = api.messages.search(q='хочу играть',count=100) # Кодовое слово для начала игры
-            break
-        except Exception as e:
-            print('Исключение!',e)
-            time.sleep(2)
-    for i in messages[1:]: # Пока не отработаем все старые сообщения, новые не принимаем
-        player,time = i['uid'], i['date'] # id игрока и время сообщения
-        if player not in users_time.keys(): # Если игрок еще никогда не играл
-            users_time[player] = 0
-        if users_time[player] < time: # Если это не спам, то начинаем играть
-            out = open('talking.txt','w')
-            out.write('gameBegan with '+str(player)) # Лог by Игорь
-            out.close()
-            record = open('bot2.txt','w') # Пишем в файл id игрока для мэина
-            record.write(str(player))
-            record.close()
-            serv = threading.Thread(target=st_serv, name="st_serv") # стартуем сервер
-            serv.start()
-            bot = threading.Thread(target=game, name="game") # стартуем бота
-            bot.start()
-            serv.join() # Ждем завершения всех потоков
-            bot.join()
-            users_file = open('users.txt','w') # Переписываем весь файл. TODO: Более элегантное решение
-            users_time[player] = i['date'] # Обновляем время последней игры
-            for ids in users_time.keys():
-                users_file.write(str(ids)+' '+str(users_time[ids]))
-            users_file.close()
+        while True:
+            try:
+                messages = api.messages.search(q='хочу играть',count=100) # Кодовое слово для начала игры
+                break
+            except Exception as e:
+                print('Исключение!',e)
+                time.sleep(2)
+        for i in messages[-2::-1]: # Пока не отработаем все старые сообщения, новые не принимаем
+            player,time = i['uid'], i['date'] # id игрока и время сообщения
+            if player not in users_time.keys(): # Если игрок еще никогда не играл
+                users_time[player] = 0
+            if users_time[player] < time: # Если это не спам, то начинаем играть
+                out = open('talking.txt','w')
+                out.write('gameBegan with '+str(player)) # Лог by Игорь
+                out.close()
+                record = open('bot2.txt','w') # Пишем в файл id игрока для мэина
+                record.write(str(player))
+                record.close()
+                serv = threading.Thread(target=st_serv, name="st_serv") # стартуем сервер
+                serv.start()
+                bot = threading.Thread(target=game, name="game") # стартуем бота
+                bot.start()
+                serv.join() # Ждем завершения всех потоков
+                bot.join()
+                users_file = open('users.txt','w') # Переписываем весь файл. TODO: Более элегантное решение
+                users_time[player] = i['date'] # Обновляем время последней игры
+                for ids in users_time.keys():
+                    users_file.write(str(ids)+' '+str(users_time[ids]))
+                users_file.close()
+except KeyboardInterrupt or SystemExit: # Если вы решили выйти
+    os.remove('bot2.txt')
+    for ids in users_time.keys():
+        users_file.write(str(ids) + ' ' + str(users_time[ids]))
+    users_file.close()
+    users_file = open('users.txt', 'w')
