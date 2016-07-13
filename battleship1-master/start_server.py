8c5806e250336bd128b204fcd1e809da9e1fac53
Sergey Beglov <doktorkrab@github.com>
2016-07-13 09:01:12 +0300

    Обработка исключений

--- battleship1-master/start_server.py
+++ battleship1-master/start_server.py
@@ -7,18 +7,34 @@ token = open('token_server.txt','r')
 api = vk.API(vk.Session(access_token=token.read()))
 token.close()
 # Чтение всех пользователей, даты последней игры. "Антиспам"
-users_file = open('users.txt','r')
-users_time = {}
-reads = ' '
-while reads != '': # While not eof
-    reads = users_file.readline()
-    user,time = reads.split(' ')
-    users_time[int(user)] = int(time)
-users_file.close()
+try:
+    users_file = open('users.txt','r')
+    users_time = {}
+    reads = ' '
+    while reads != '': # While not eof
+        reads = users_file.readline()
+        user,time = reads.split(' ')
+        users_time[int(user)] = int(time)
+    users_file.close()
+except FileNotFoundError:
+    users_time = {}
+    print('Файл не найден!')
+    users_file = open('users.txt','w') #
+    users_file.write('')               # Создаем файл, если его не существует
+    users_file.close()                 #
+except Exception as e:
+    print('Необработанное исключение!\n Текст:\n',sys.exc_info()[0]) # Защита от вылетов
 while True:
-    messages = api.messages.search(q='хочу играть',count=200) # Кодовое слово для начала игры
+    while True:
+        try:
+            messages = api.messages.search(q='хочу играть',count=200) # Кодовое слово для начала игры
+            break
+        except Exception as e:
+            print('Исключение!',sys.exc_info()[0])
     for i in messages['items']: # Пока не отработаем все старые сообщения, новые не принимаем
         player,time = i['user_id'], i['time'] # id игрока и время сообщения
+        if player not in users_time.keys(): # Если игрок еще никогда не играл
+            users_time[player] = 0
         if users_time[player] < time: # Если это не спам, то начинаем играть
             out = open('talking.txt','w')
             out.write('gameBegan with '+str(player)) # Лог by Игорь
